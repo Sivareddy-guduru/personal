de9dd990f3bedf273d527598d9bf5c5ef53519b0
Sivareddy-guduru <sivareddy4guduru@gmail.com>
2018-04-08 15:39:07 +0800

    Update TwoCircles.java

--- src/com/techolution/puzzles/TwoCircles.java
+++ src/com/techolution/puzzles/TwoCircles.java
@@ -4,56 +4,32 @@ package com.techolution.puzzles;
  * Created by sivareddy.guduru on 16/3/18.
  */
 public class TwoCircles {
-     private static final String TOUCHING = "Touching";
-    private static final String DISJOINT_INSIDE = "Disjoint-Inside";
-    private static final String DISJOINT_OUTSIDE = "Disjoint-Outside";
-    private static final String CONCENTRIC = "Concentric";
-    private static final String INTERSECTING = "Intersecting";
-
-
-    static String[] circles(String[] info) {
-
+   static String[] circles(String[] info) {
         String[] result = new String[info.length];
         for (int i = 0; i < info.length; i++) {
-            String[] lines = info[i].split(" ");
+            String[] dataList = info[i].split(" ");
+            int x1 = Integer.parseInt(dataList[0]);
+            int y1 = Integer.parseInt(dataList[1]);
+            int r1 = Integer.parseInt(dataList[2]);
 
-            int x1 = Integer.valueOf(lines[0]);
-            int y1 = Integer.valueOf(lines[1]);
-            int x2 = Integer.valueOf(lines[3]);
-            int y2 = Integer.valueOf(lines[4]);
-            int r1 = Integer.valueOf(lines[2]);
-            int r2 = Integer.valueOf(lines[5]);
+            int x2 = Integer.parseInt(dataList[3]);
+            int y2 = Integer.parseInt(dataList[4]);
+            int r2 = Integer.parseInt(dataList[5]);
 
-            if (x1 == 0) {
-
-                if (y1 == y2) {
-                    result[i] = CONCENTRIC;
-                } else if (Math.abs(y1 - y2) > (r1 + r2)) {
-                    result[i] = DISJOINT_OUTSIDE;
-                } else if ((Math.abs(y1 - y2) + r1 < r2) || (Math.abs(y1 - y2) + r2 < r1)) {
-                    result[i] = DISJOINT_INSIDE;
-                } else if (Math.abs(y1 - y2) > Math.abs(r1 - r2)) {
-                    result[i] = INTERSECTING;
-                } else if (Math.abs(y1 - y2) == Math.abs(r1 - r2)) {
-                    result[i] = TOUCHING;
-                }
-
-            } else {
-                if (x1 == x2) {
-                    result[i] = CONCENTRIC;
-                } else if (Math.abs(x1 - x2) > (r1 + r2)) {
-                    result[i] = DISJOINT_OUTSIDE;
-                } else if ((Math.abs(x1 - x2) + r1 < r2) || (Math.abs(x1 - x2) + r2 < r1)) {
-                    result[i] = DISJOINT_INSIDE;
-                } else if (Math.abs(x1 - x2) > Math.abs(r1 - r2)) {
-                    result[i] = INTERSECTING;
-                } else if (Math.abs(x1 - x2) == Math.abs(r1 - r2)) {
-                    result[i] = TOUCHING;
-                }
-
+            int dis = (int) Math.sqrt((Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+            if (dis == (r1 + r2) || dis == (r2 - r1)) {
+                result[i] = "Touching";
+            } else if (dis == 0) {
+                result[i] = "Concentric";
+            } else if (dis > (r1 + r2)) {
+                result[i] = "Disjoint-Outside";
+            } else if (dis <= Math.abs((r2 - r1))) {
+                result[i] = "Disjoint-Inside";
+            } else if (dis <= (r1 + r2)) {
+                result[i] = "Intersecting";
             }
-
         }
+
         return result;
     }
 }
