ff8a088b99318e2ef57fb6f2535544e3e1cd0916
Sivareddy-guduru <sivareddy4guduru@gmail.com>
2018-04-08 15:24:45 +0800

    Update InTheFuture.java

--- src/com/techolution/puzzles/InTheFuture.java
+++ src/com/techolution/puzzles/InTheFuture.java
@@ -6,17 +6,11 @@ package com.techolution.puzzles;
 public class InTheFuture {
 
     static int minNum(int A, int K, int P) {
-
-        int count = 1;
         if (A >= K) {
             return -1;
         }
-        while (K <= A + P) {
-            A += A;
-            K += K;
-            count++;
-        }
-        return count;
+        int days = (P / (K - A)) + 1;
+        return days;
     }
 
 }
